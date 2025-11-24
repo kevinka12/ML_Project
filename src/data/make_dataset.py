4a1e8ef426d3fa4d4a5d22724d5fd9181f74b66a
Kevin <kevinkirkka@gmail.com>
2025-11-24 09:23:03 +0100

    feat(data): add artifact directory creation function

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,13 @@
 # -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import os
+# Same notebook variables
+max_date = "2024-01-31"
+min_date = "2024-01-01"
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+def create_artifact_directory():
     """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
-
-
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    Creates artifacts/ directory if it does not exist.
+    """
+    os.makedirs("artifacts", exist_ok=True)
+    print("Created artifacts directory")
