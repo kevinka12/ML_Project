23d8fdd3aed5086eaaa4f8cc01d78feba430f3d8
Nathalie <nathalienoerby@outlook.com>
2025-12-02 11:22:53 +0100

    feat(models): add artifact saving for column list and model results

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -256,3 +256,24 @@ def train_logistic_regression(
         y_pred_test,
         model_results
     )
+
+
+import json
+from pprint import pprint
+
+def save_artifacts(X_train, model_results):
+    # Save column list
+    column_list_path = './artifacts/columns_list.json'
+    with open(column_list_path, 'w+') as columns_file:
+        columns = {'column_names': list(X_train.columns)}
+        pprint(columns)
+        json.dump(columns, columns_file)
+
+    print('Saved column list to ', column_list_path)
+
+    # Save model results
+    model_results_path = "./artifacts/model_results.json"
+    with open(model_results_path, 'w+') as results_file:
+        json.dump(model_results, results_file)
+
+    return column_list_path, model_results_path
