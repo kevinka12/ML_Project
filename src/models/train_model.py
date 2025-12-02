ae60a9624fb1b343fe006900bafb6a2029e53517
Nathalie <nathalienoerby@outlook.com>
2025-12-02 11:41:57 +0100

    feat(models): add function to retrieve production model from MLflow Model Registry

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -342,3 +342,37 @@ def select_best_model(experiment_name):
     print(f"Best model: {best_model}")
 
     return experiment_best, best_model, results_df
+
+
+
+from mlflow.tracking import MlflowClient
+
+def get_production_model(model_name):
+    client = MlflowClient()
+    
+    # Find model versions in Production stage
+    prod_model = [
+        model
+        for model in client.search_model_versions(f"name='{model_name}'")
+        if dict(model)['current_stage'] == 'Production'
+    ]
+    
+    prod_model_exists = len(prod_model) > 0
+    
+    if prod_model_exists:
+        prod_model_version = dict(prod_model[0])['version']
+        prod_model_run_id = dict(prod_model[0])['run_id']
+        
+        print('Production model name: ', model_name)
+        print('Production model version:', prod_model_version)
+        print('Production model run id:', prod_model_run_id)
+        
+        return {
+            "model_name": model_name,
+            "model_version": prod_model_version,
+            "run_id": prod_model_run_id
+        }
+    
+    else:
+        print('No model in production')
+        return None
