b400674ef9f841f196eaad319026d8e967c3f581
Nathalie <nathalienoerby@outlook.com>
2025-12-02 10:43:06 +0100

    feat(models): add XGBoost training with RandomizedSearchCV to training pipeline

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -49,3 +49,34 @@ def split_train_test(data):
     )
 
     return X_train, X_test, y_train, y_test
+
+
+
+from xgboost import XGBRFClassifier
+from sklearn.model_selection import RandomizedSearchCV
+from scipy.stats import uniform, randint
+
+def train_xgboost_model(X_train, y_train):
+    model = XGBRFClassifier(random_state=42)
+
+    params = {
+        "learning_rate": uniform(1e-2, 3e-1),
+        "min_split_loss": uniform(0, 10),
+        "max_depth": randint(3, 10),
+        "subsample": uniform(0, 1),
+        "objective": ["reg:squarederror", "binary:logistic", "reg:logistic"],
+        "eval_metric": ["aucpr", "error"]
+    }
+
+    model_grid = RandomizedSearchCV(
+        model, 
+        param_distributions=params, 
+        n_jobs=-1, 
+        verbose=3, 
+        n_iter=10, 
+        cv=10
+    )
+
+    model_grid.fit(X_train, y_train)
+
+    return model_grid
